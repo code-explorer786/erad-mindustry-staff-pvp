6d313b726ab65c878e3d616470ffae3bf2c274fa
code-explorer786 <68312688+code-explorer786@users.noreply.github.com>
2024-10-27 13:43:39 +0700

    bootstrap

---
+++ src/staffpvp/StaffPVP.java
@@ -0,0 +1,18 @@
+package staffpvp;
+
+import arc.*;
+import arc.util.*;
+import mindustry.*;
+import mindustry.content.*;
+import mindustry.game.EventType.*;
+import mindustry.gen.*;
+import mindustry.mod.*;
+import mindustry.ui.dialogs.*;
+
+public class StaffPVP extends Plugin{
+
+    public StaffPVP(){
+
+    }
+
+}
